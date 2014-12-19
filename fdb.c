60c8c9da64976f48481f78978e07b7451feab634
David Ulrich <dulrich@atomicinfotech.com>
2014-12-19 11:26:05 -0600

    bring in more mysql helpers

--- fdb.c
+++ fdb.c
@@ -15,7 +15,83 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <my_global.h>
+#include <mysql.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+static char* FDB_DB_HOST = "localhost";
+static char* FDB_DB_USER = "root";
+static char* FDB_DB_PASS = "34dS&9ee";
+static char* FDB_DB_NAME = "fdb";
+
+
+/* ===== MYSQL HELPER FNS ===== */
+int fdb_mysql_close();
+int fdb_mysql_error();
+int fdb_mysql_query(char* query);
+MYSQL_RES* fdb_mysql_query_res(char* query);
+
+MYSQL* fdb_conn;
+
+int fdb_mysql_close() {
+	mysql_close(fdb_conn);
+	fdb_conn = NULL;
+	return 0;
+}
+
+int fdb_mysql_error() {
+	if (fdb_conn == NULL) return 1;
+	
+	fprintf(stderr, "%s\n", mysql_error(fdb_conn));
+	return 1;
+}
+
+int fdb_mysql_query(char* query) {
+	MYSQL_RES* result = fdb_mysql_query_res(query);
+	
+	if (result != NULL) mysql_free_result(result);
+	
+	return 0;
+}
+
+MYSQL_RES* fdb_mysql_query_res(char* query) {
+	if (fdb_conn == NULL) {
+		fdb_conn = mysql_init(NULL);
+	
+		if (fdb_conn == NULL) {
+			fprintf(stderr, "%s\n", mysql_error(fdb_conn));
+			return NULL;
+		}
+		
+		if (mysql_real_connect(fdb_conn,FDB_DB_HOST,FDBD_DB_USER,FDB_DB_PASS,FDB_DB_NAME,0,NULL,0) == NULL) {
+			fdb_mysql_error(fdb_conn);
+			return NULL;
+		}
+	}
+	
+	if (mysql_query(fdb_conn, query)) {
+		fdb_mysql_error(fdb_conn);
+		return NULL;
+	}
+	
+	MYSQL_RES* result = mysql_store_result(fdb_conn);
+	
+	if (result == NULL) {
+		fdb_mysql_error(fdb_conn);
+	}
+	
+	return result;
+}
+
+
 
 
 /* ===== REGULAR HELPER FNS ===== */
@@ -58,6 +134,8 @@ int do_input() {
 void print_options() {
 	printf("1: view balance, 2: add debit or credit, 3: create budget, x: exit\n");
 }
+
+
 
 
 /* ===== MAIN ===== */
